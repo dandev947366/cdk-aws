e4c32068cd466c393c0b6cd24a00647812df7b25
Dan <dan.dev947366@gmail.com>
2025-03-30 19:13:43 +0300

    feat(photo): add PhotoStack and split task id

---
+++ starter-cdk/lib/PhotosStack.ts
@@ -0,0 +1,19 @@
+import * as cdk from "aws-cdk-lib";
+import { Fn } from "aws-cdk-lib";
+import { Bucket, CfnBucket } from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
+export class PhotosStack extends cdk.Stack {
+  private stackSuffix: string;
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    this.initializeSuffix()
+    new Bucket(this, "PhotosBucket", {
+        bucketName: `photos-bucket-${this.stackSuffix}`
+    });
+  }
+  // splitting stack id
+  private initializeSuffix() {
+    const shortStackId = Fn.select(2, Fn.split('/', this.stackId))
+    this.stackSuffix = Fn.select(4, Fn.split('-', shortStackId))
+  }
+}
