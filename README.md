b12b081ef985b64155647eb543c0be31f4acdf64
Dan <dan.dev947366@gmail.com>
2025-03-30 15:43:47 +0300

    feat(starter) : init cdk starter

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # cdk-aws
  Serverless app with Typescript and AWS CDK
+
+## Tech stack
+* Nodejs
+* Typescript
+* AWS account
+
+## Useful commands
+
+* `npm run build`   compile typescript to js
+* `npm run watch`   watch for changes and compile
+* `npm run test`    perform the jest unit tests
+* `cdk deploy`      deploy this stack to your default AWS account/region
+* `cdk diff`        compare deployed stack with current state
+* `cdk synth`       emits the synthesized CloudFormation template
+
+## Packge version 
+Node : v18.12.1
+Typescript : v4.9.5
+AWS CDK : v2.1006.0
---
+++ starter-cdk/test/starter-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as StarterCdk from '../lib/starter-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/starter-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new StarterCdk.StarterCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
