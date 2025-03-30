94176e7c697bda6ca761579d1e97c9af0701730c
Dan <dan.dev947366@gmail.com>
2025-03-30 19:37:24 +0300

    feat(photo): add PhotoHandlerStack

--- starter-cdk/bin/starter-cdk.ts
+++ starter-cdk/bin/starter-cdk.ts
@@ -2,6 +2,9 @@
 import * as cdk from 'aws-cdk-lib';
 import { StarterCdkStack } from '../lib/starter-cdk-stack';
 import { PhotosStack } from '../lib/PhotosStack';
+import { PhotosHandlerStack } from '../lib/PhotoHandlerStack';
 
 const app = new cdk.App();
 new PhotosStack(app, 'PhotosStack')
+new PhotosHandlerStack(app, 'PhotosHandlerStack')
+
--- starter-cdk/lib/PhotosStack.ts
+++ starter-cdk/lib/PhotosStack.ts
@@ -1,5 +1,5 @@
 import * as cdk from "aws-cdk-lib";
-import { Fn } from "aws-cdk-lib";
+import { CfnOutput, Fn } from "aws-cdk-lib";
 import { Bucket, CfnBucket } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 export class PhotosStack extends cdk.Stack {
@@ -7,9 +7,13 @@ export class PhotosStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     this.initializeSuffix()
-    new Bucket(this, "PhotosBucket", {
+    const photosBucket = new Bucket(this, "PhotosBucket", {
         bucketName: `photos-bucket-${this.stackSuffix}`
     });
+    new CfnOutput(this, 'photos-bucket', {
+        value : photosBucket.bucketArn,
+        exportName: 'photos-bucket'
+    })
   }
   // splitting stack id
   private initializeSuffix() {
