6ff3d8577116b071503e2f57d4fd98aca83ba379
Dan <dan.dev947366@gmail.com>
2025-03-30 16:44:44 +0300

    feat(construct): Create cdk construct buckets

--- starter-cdk/lib/starter-cdk-stack.ts
+++ starter-cdk/lib/starter-cdk-stack.ts
@@ -1,13 +1,46 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Bucket, CfnBucket } from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+class L3Bucket extends Construct {
+  constructor(scope: Construct, id: string, expiration: number) {
+    super(scope, id);
+    new Bucket(this, "L3Bucket", {
+      lifecycleRules: [
+        {
+          expiration: cdk.Duration.days(2),
+        },
+      ],
+    });
+  }
+}
 export class StarterCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
+    // Create s3 bucket 3 ways
+    // this = StarterCdkStack
+    // L1 construct
+    new CfnBucket(this, "MyL1Bucket", {
+      lifecycleConfiguration: {
+        rules: [
+          {
+            expirationInDays: 1,
+            status: "Enabled",
+          },
+        ],
+      },
+    });
+    // L2 construct : most popular
+    new Bucket(this, "MyL2Bucket", {
+      lifecycleRules: [
+        {
+          expiration: cdk.Duration.days(2),
+        },
+      ],
+    });
+    // L3 construct
+    new L3Bucket(this, "MyL3Bucket", 3);
     // example resource
     // const queue = new sqs.Queue(this, 'StarterCdkQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
